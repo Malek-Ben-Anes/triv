e8f4d9fdcdaf34ff99b20bc9a968be481e2eb028
Omar-86 <benanesmalek@gmail.com>
2021-01-28 19:45:21 +0100

    first commit

---
+++ src/main/java/com/trivago/searchcore/casestudy/HotelDealsApplication.java
@@ -0,0 +1,13 @@
+package com.trivago.searchcore.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelDealsApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HotelDealsApplication.class, args);
+    }
+
+}
